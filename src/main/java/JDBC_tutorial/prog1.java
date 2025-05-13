79286f2b1392c37a275487f89df55f81bc99ffa5
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-05-14 00:10:37 +0530

    How a database connected and created and data is retrieved

--- src/main/java/JDBC_tutorial/prog1.java
+++ src/main/java/JDBC_tutorial/prog1.java
@@ -7,6 +7,7 @@ public class prog1 {
         String url="jdbc:mysql://localhost:3306/students";
         String username="root";
         String password= "123456";
+        String query="Select * from Employees";
 
 
         try{
@@ -20,6 +21,30 @@ public class prog1 {
     try{
         Connection con =DriverManager.getConnection(url,username,password);
         System.out.println("Connection etablished Successfully");
+
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery(query);
+
+        while(rs.next()){
+            int id=rs.getInt("id");
+            String name=rs.getString("name");
+            String job_title=rs.getString("job_title");
+            double salary=rs.getDouble("salary");
+            System.out.println();
+
+            System.out.println("--------------------------------");
+            System.out.println("ID="+id);
+            System.out.println("Name="+name);
+            System.out.println("Job Title="+job_title);
+            System.out.println("Salary="+salary);
+
+        }
+        rs.close();
+        stmt.close();
+        con.close();
+        System.out.println("Connection Closed Successfully");
+
+
     } catch (SQLException e) {
         System.out.println(e.getMessage());;
     }
